066030e9d3a4ef2e2ed83c8d87e76dd937d5cf49
lorax99 <113378771+lorax99@users.noreply.github.com>
2025-10-25 10:19:58 +0200

    added ayun image to map

--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
-const map = L.map('map').setView([51.505, -0.09], 13);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+// Coordinates in CRS.Simple take the form of [y, x] instead of [x, y].
+const AYUN_MAP_HEIGHT = 2483;
+const AYUN_MAP_WIDTH = 3513;
+const AYUN_MAP_RATIO = AYUN_MAP_HEIGHT / AYUN_MAP_WIDTH;
+
+//#region Map
+// TODO: fix the bounds
+const bounds = [[0,0], [2483, 3513]];
+
+const map = L.map('map', {
+    crs: L.CRS.Simple,
+    minZoom: -1.7,
+    maxBounds: bounds
+});
+const image = L.imageOverlay("assets/Ayun Map Aktuell.jpg", bounds).addTo(map)
+
+map.fitBounds(bounds);
 
 // TODO: https://ruibarreira.github.io/jvectormap-svg-converter/
