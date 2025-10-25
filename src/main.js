62b14f8387a5cb4e2ee5e71080cd5ab53cc27025
lorax99 <113378771+lorax99@users.noreply.github.com>
2025-10-25 09:39:14 +0200

    reorganized some files

---
+++ src/main.js
@@ -0,0 +1,7 @@
+const map = L.map('map').setView([51.505, -0.09], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+// TODO: https://ruibarreira.github.io/jvectormap-svg-converter/
